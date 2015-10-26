bdca10a9fbd028a12508ed41801ac76381819031
christophekindt <christophe.kindt@telenet.be>
2015-10-26 16:08:43 +0100

    Routing aangepast, werkt nog niet

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,100 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 
+var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+router.param('post', function (req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec( function (err, post) {
+		if (err) { return next(err); }
+		if (!post) {
+			return next('Could not find post');
+		}
+
+		req.post = post;
+		return next();
+	});
+});
+
+router.param('comment', function (req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec( function(err, comment) {
+		if (err) { return next(err); }
+		if (!comment) {
+			return ('Could not find comment');
+		}
+
+		req.comment = comment;
+		return next();
+	});
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Flapper News' });
 });
 
-/* Posts */
-router.get('/posts', function(req, res, next){
-  Post.find(function(err, posts){
-    if(err){next(err);}
-    res.json(posts);
-  });
+/* GET posts */
+router.get('/posts', function(req, res, next) {
+	Post.find(function (err, posts) {
+		if (err) { return next(err); }
+		res.json(posts);
+	});
 });
 
-router.post('/posts', function(req, res, next){
-  var post = new Post(req.body);
-  post.save(function(err, post){
-    if(err){next(err);}
-    res.json(post);
-  });
+/* POST new post */
+router.post('/posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function (err, post) {
+		if (err) { return next(err); }
+
+		res.json(post);
+	});
 });
+
+/* GET post
+   Obtenemos un post (Se ejecuta automáticamente la query definida en router.param)
+ */
+router.get('/posts/:post', function (req, res) {
+	req.post.populate('comments', function(err, post) {
+		res.json(post);
+	});
+});
+
+/* PUT Upvote a post */
+router.put('/posts/:post/upvote', function (req, res, next) {
+	req.post.upvote( function (err, post) {
+		if (err) { return next(err); }
+		res.json(post);
+	});
+});
+
+/* POST new comment */
+router.post('/posts/:post/comments', function (req, res, next) {
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save( function (err, comment) {
+		if (err) { return next(err); }
+		req.post.comments.push(comment);
+		req.post.save( function (err, post) {
+			if (err) { return next(err); }
+			res.json(comment);
+		});
+	});
+});
+
+/* Upvote a comment */
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+	req.comment.upvote( function (err, comment) {
+		if (err) { return next(err); }
+		res.json(comment);
+	});
+});
+
 module.exports = router;
